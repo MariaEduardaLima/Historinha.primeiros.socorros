ca8b1ced17c765579c61f90aecaca09aac1dd5b8
Maria Eduarda <106595568+MariaEduardaLima@users.noreply.github.com>
2024-08-14 11:14:12 -0300

    .

--- js/perguntas.js
+++ js/perguntas.js
@@ -1,6 +1,6 @@
 export const perguntas = [
     {
-        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
+        enunciado: "O que você acha de estar aprendendo sobre primeiros socorros através de uma tela?",
         alternativas: [
             {
                 texto: "Isso é assustador!",
