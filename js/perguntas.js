900b967f323a9a7547e46f8b73a607cfa9e73b64
Maria Eduarda <106595568+MariaEduardaLima@users.noreply.github.com>
2024-08-14 12:29:15 -0300

    .

--- js/perguntas.js
+++ js/perguntas.js
@@ -4,18 +4,12 @@ export const perguntas = [
         alternativas: [
             {
                 texto: "Isso é assustador!",
-                afirmacao: [
-                    "No início ficou com medo do que essa tecnologia pode fazer.",
-                    "Achou assustador pensar na velocidade na qual a tecnologia está avançando."
-                ],
+              
                 proxima: 1,
             },
             {
                 texto: "Isso é maravilhoso!",
-                afirmacao: [
-                    "Quis saber como usar IA no seu dia a dia.",
-                    "Pensou que IA pode ajudar em tarefas da sua vida."
-                ],
+               
                 proxima: 2,
             },
         ]
@@ -25,20 +19,12 @@ export const perguntas = [
         alternativas: [
             {
                 texto: "Verificar se a pessoa está respirando e tem pulso.",
-                afirmacao: [
-                    "Conseguiu utilizar a IA para buscar informações úteis.",
-                    "Percebeu que a IA pode ajudar a encontrar informações úteis na internet de forma mais rápida e direcionada.",
-                    "Percebeu que a IA consegue explicar termos complicados de forma simplificada e isso ajudou muito suas pesquisas sobre assuntos complexos."
-                ],
+                
                 proxima: 3,
             },
             {
                 texto: "Só ficar olhando.",
-                afirmacao: [
-                    "Sentiu mais facilidade em utilizar seus próprios recursos para escrever seu trabalho.",
-                    "Achou que era muito mais fácil procurar por respostas utilizando alguns meios mais tradicionais mesmo que levasse mais tempo.",
-                    "Sentiu um pouco de medo de quais dados pessoais seus a IA poderia utilizar e por isso prefere fazer suas coisas com pouca intromissão da tecnologia."
-                ],
+               
                 proxima: 4,
             },
         ]
@@ -48,20 +34,12 @@ export const perguntas = [
         alternativas: [
             {
                 texto: "Aplicar compressas quentes no pescoço da criança.",
-                afirmacao: [
-                    "Conseguiu utilizar a IA para buscar informações úteis.",
-                    "Percebeu que a IA pode ajudar a encontrar informações úteis na internet de forma mais rápida e direcionada.",
-                    "Percebeu que a IA consegue explicar termos complicados de forma simplificada e isso ajudou muito suas pesquisas sobre assuntos complexos."
-                ],
+               
                 proxima: 3,
             },
             {
                 texto: "Colocar os braços ao redor da cintura, posicionar um punho acima do umbigo e realizar compresões rápidas.",
-                afirmacao: [
-                    "Sentiu mais facilidade em utilizar seus próprios recursos para escrever seu trabalho.",
-                    "Achou que era muito mais fácil procurar por respostas utilizando alguns meios mais tradicionais mesmo que levasse mais tempo.",
-                    "Sentiu um pouco de medo de quais dados pessoais seus a IA poderia utilizar e por isso prefere fazer suas coisas com pouca intromissão da tecnologia."
-                ],
+                
                 proxima: 4,
             },
         ]
@@ -71,18 +49,12 @@ export const perguntas = [
         alternativas: [
             {
                 texto: "Garantir que o ambiente ao redor esteja seguro e monitorar a pessoa até que a convulção termine.",
-                afirmacao: [
-                    "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA.",
-                    "Participa ativamente do desenvolvimento de soluções criativas e na melhoria de processos em IA."
-                ],
+                
                 proxima: 5,
             },
             {
                 texto: "Colocar um objeto na boca da pessoa para evitar que ela morda a língua.",
-                afirmacao: [
-                    "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética.",
-                    "Criou grupos de ética voltado para IA e busca ativamente reduzir as desigualdades geradas pela automação."
-                ],
+               
                 proxima: 6,
             },
         ]
@@ -92,18 +64,12 @@ export const perguntas = [
         alternativas: [
             {
                 texto: "Aplicar uma bandagem limpa diretamente sobre a ferida e pressionar firmemente.",
-                afirmacao: [
-                    "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA.",
-                    "Participa ativamente do desenvolvimento de soluções criativas e na melhoria de processos em IA."
-                ],
+                
                 proxima: 5,
             },
             {
                 texto: "Lavar com água fria.",
-                afirmacao: [
-                    "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética.",
-                    "Criou grupos de ética voltado para IA e busca ativamente reduzir as desigualdades geradas pela automação."
-                ],
+                
                 proxima: 6,
             },
         ]
@@ -113,18 +79,12 @@ export const perguntas = [
         alternativas: [
             {
                 texto: "Manter a pessoa em pé até a ajuda chegar.",
-                afirmacao: [
-                    "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA.",
-                    "Participa ativamente do desenvolvimento de soluções criativas e na melhoria de processos em IA."
-                ],
+                
                 proxima: 7,
             },
             {
                 texto: "Colocar a pessoa de lado, dobrar a perna superior e apoiar o braço superior sobre a perna.",
-                afirmacao: [
-                    "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética.",
-                    "Criou grupos de ética voltado para IA e busca ativamente reduzir as desigualdades geradas pela automação."
-                ],
+                
                 proxima: 7,
             },
         ]
@@ -134,18 +94,12 @@ export const perguntas = [
         alternativas: [
             {
                 texto: "Dar analgésicos sem orientação médica.",
-                afirmacao: [
-                    "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA.",
-                    "Participa ativamente do desenvolvimento de soluções criativas e na melhoria de processos em IA."
-                ],
+                
                 proxima: 7,
             },
             {
                 texto: "Imobilizar a área afetada e evitar movimentar a parte fraturada.",
-                afirmacao: [
-                    "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética.",
-                    "Criou grupos de ética voltado para IA e busca ativamente reduzir as desigualdades geradas pela automação."
-                ],
+                
                 proxima: 7,
             },
         ]
@@ -155,18 +109,11 @@ export const perguntas = [
         alternativas: [
             {
                 texto: "Forçá-la a comer algo doce.",
-                afirmacao: [
-                    "Notou também que muitas pessoas não sabem ainda utilizar as ferramentas tradicionais e decidiu compartilhar seus conhecimentos de design utilizando ferramentas de pintura digital para iniciantes.",
-                    "Ainda acha que os meios de desenho tradicionais são mais eficazes para a criatividade, por isso vem estimulando pessoas em suas redes sociais a fazer pintura em aquarela."
-                ],
+               
             },
             {
                 texto: "Manter a pessoa deitada, elevar as pernas e cobri-la para manter o calor.",
-                afirmacao: [
-                    "Acelerou o processo de criação de trabalhos utilizando geradores de imagem e agora consegue ensinar pessoas que sentem dificuldades em desenhar manualmente como utilizar também!",
-                    "Compartilhou artes em redes sociais como forma de ensinar como se comunicar através da arte.",
-                    "Percebeu que muitas pessoas têm dificuldade em expressar suas ideias desenhando e acha que a IA é capaz de empoderar essas pessoas a tirarem ideias do papel."
-                ],
+                
             },
         ]
     }
